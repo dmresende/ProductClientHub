9d8f2f2d4d4fd8f8d3c021905800d9b0e20b2ac3
Douglas Resende <Douglas Resende@DESKTOP-OV2STEO>
2025-04-10 17:30:04 -0300

    Add project files.

---
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.API.UseCases.Clients.Register;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
+        public IActionResult Register([FromBody]RequestClientJson request)
+        {
+            try
+            {
+                var response = new RegisterClientUseCase();
+                response.Execute(request);
+
+                return Created(string.Empty, response);
+
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new ResponseErrorMessagesJson(ex.Message));
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorMessagesJson("Unknown error."));
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute]Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Deçete()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ProductClientHub.API/UseCases/Clients/Register/RegisterClientValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ProductClientHub.Communication.Requests;
+
+namespace ProductClientHub.API.UseCases.Clients.Register
+{
+    public class RegisterClientValidator : AbstractValidator<RequestClientJson>
+    {
+        public RegisterClientValidator()
+        {
+            RuleFor(client => client.Name).NotEmpty().WithMessage("Name cannot be empty");
+            RuleFor(client => client.Email).EmailAddress().WithMessage("Invalid email!");
+        }
+    }
+}
