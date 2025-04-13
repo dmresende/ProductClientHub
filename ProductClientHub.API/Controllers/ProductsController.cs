e5af4ce24f8a611bf475a587ca443834ed236899
Douglas Resende <Douglas Resende@DESKTOP-OV2STEO>
2025-04-12 23:34:45 -0300

    feat(Product) : Register product in client

---
+++ ProductClientHub.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.API.UseCases.Products.Register;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        [Route("{clientId}")]
+        [ProducesResponseType(typeof(ResponseShortProductJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status404NotFound)]
+        public IActionResult Register([FromRoute] Guid clientId, RequestProductJson request)
+        {
+            var response = new RegisterProductUseCase();
+            response.Execute(clientId, request);
+
+            return Created(string.Empty, response);
+        }
+    }
+}
---
+++ ProductClientHub.API/UseCases/Products/SharedValidator/RequestProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using ProductClientHub.Communication.Requests;
+
+namespace ProductClientHub.API.UseCases.Product.SharedValidator
+{
+    public class RequestProductValidator : AbstractValidator<RequestProductJson>
+    {
+        public RequestProductValidator()
+        {
+            RuleFor(product => product.Name).NotEmpty().WithMessage("Name cannot be empty");
+            RuleFor(product => product.Brand).NotEmpty().WithMessage("Brand cannot be empty");
+            RuleFor(product => product.Price).GreaterThan(0).WithMessage("Price value is invalid");
+        }
+    }
+}
